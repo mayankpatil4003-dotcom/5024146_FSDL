804cc1982d9b8739a0fc6a33c6eba89bf9b81a02
mayankpatil4003-dotcom <mayankpatil4003@gmail.com>
2026-02-18 06:37:26 +0000

    done

---
+++ week04_18022026/second.js
@@ -0,0 +1,21 @@
+// test2.js
+
+// FUNCTION
+function greetUser(username) {
+    console.log("Welcome " + username);
+}
+
+// CALLING FUNCTION
+greetUser("Alison");
+
+// LOOP
+for (let i = 1; i <= 5; i++) {
+    console.log("Number: " + i);
+}
+
+// WHILE LOOP
+let count = 1;
+while (count <= 3) {
+    console.log("Count is: " + count);
+    count++;
+}
