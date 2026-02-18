8463660f6f89aa3c17e8f582270cf973a9c7e842
mayankpatil4003-dotcom <mayankpatil4003@gmail.com>
2026-02-18 06:37:08 +0000

    NEW

--- week04_18022026/first.js
+++ week04_18022026/first.js
@@ -1,1 +1,19 @@
-console.log("First Program");
+// test1.js
+
+// VARIABLES
+let name = "Mayank";
+let age = 19;
+
+// OPERATORS
+let yearOfBirth = 2025 - age;
+
+// PRINTING OUTPUT
+console.log("Hello my name is " + name);
+console.log("I was born in " + yearOfBirth);
+
+// CONDITIONALS
+if (age >= 18) {
+    console.log("I am an adult.");
+} else {
+    console.log("I am a minor.");
+}
